092816e46f76031c9660a47afb609869ced9edbf
chenzhaohua <chenzhaohua@chenzhaohuadeMacBook-Pro.local>
2015-12-20 13:21:48 +0800

    优化代码

--- app/src/main/java/com/chen/scangon/helper/ScanGunKeyEventHelper.java
+++ app/src/main/java/com/chen/scangon/helper/ScanGunKeyEventHelper.java
@@ -11,10 +11,14 @@ import java.util.ArrayList;
  */
 public class ScanGunKeyEventHelper {
 
+    //延迟500ms，判断扫码是否完成。
     private final static long MESSAGE_DELAY = 500;
-    ArrayList<String> mBarcodeArray = new ArrayList<String>();
-    int mIndex;
+    //扫码内容
+    private StringBuffer mStringBufferResult = new StringBuffer();
+    //大小写区分
     private boolean mCaps;
+    private OnScanSuccessListener mOnScanSuccessListener;
+    private Handler mHandler = new Handler();
 
     private final Runnable mScanningFishedRunnable = new Runnable() {
         @Override
@@ -23,67 +27,98 @@ public class ScanGunKeyEventHelper {
         }
     };
 
+    //返回扫描结果
     private void performScanSuccess() {
-        String barcode = stringBuffer.toString();
+        String barcode = mStringBufferResult.toString();
         if (mOnScanSuccessListener != null)
             mOnScanSuccessListener.onScanSuccess(barcode);
-        stringBuffer.setLength(0);
+        mStringBufferResult.setLength(0);
     }
 
-    Handler mHandler = new Handler();
-
-    private OnScanSuccessListener mOnScanSuccessListener;
-
-    StringBuffer stringBuffer = new StringBuffer();
-
-
+    //key事件处理
     public void analysisKeyEvent(KeyEvent event) {
+
         int keyCode = event.getKeyCode();
-        boolean down = event.getAction() == KeyEvent.ACTION_DOWN;
-        if (down) {
-            char aChar = 0;
-            if (keyCode >= KeyEvent.KEYCODE_A && keyCode <= KeyEvent.KEYCODE_Z) {
-                aChar = (char) ((mCaps ? 'A' : 'a') + keyCode - KeyEvent.KEYCODE_A);
-            } else if (keyCode >= KeyEvent.KEYCODE_0 && keyCode <= KeyEvent.KEYCODE_9) {
-                aChar = (char) ('0' + keyCode - KeyEvent.KEYCODE_0);
-            } else {
-                switch (keyCode) {
-                    case KeyEvent.KEYCODE_SHIFT_RIGHT:
-                    case KeyEvent.KEYCODE_SHIFT_LEFT:
-                        mCaps = true;
-                        break;
-                    case KeyEvent.KEYCODE_PERIOD:
-                        aChar = '.';
-                        break;
-                    case KeyEvent.KEYCODE_MINUS:
-                        aChar = mCaps ? '_' : '-';
-                        break;
-                    case KeyEvent.KEYCODE_SLASH:
-                        aChar = '/';
-                        break;
-                    case KeyEvent.KEYCODE_BACKSLASH:
-                        aChar = mCaps ? '|' : '\\';
-                        break;
-                    case KeyEvent.KEYCODE_ENTER:
-                        mHandler.removeCallbacks(mScanningFishedRunnable);
-                        mHandler.post(mScanningFishedRunnable);
-                    default:
-                        return;
 
+        //字母大小写判断
+        checkLetterStatus(event);
 
-                }
-            }
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+
+            char aChar = getInputCode(event);;
+
             if (aChar != 0) {
-                stringBuffer.append(aChar);
+                mStringBufferResult.append(aChar);
             }
-            mHandler.removeCallbacks(mScanningFishedRunnable);
-            mHandler.postDelayed(mScanningFishedRunnable, MESSAGE_DELAY);
-        } else {
-            if (keyCode == KeyEvent.KEYCODE_SHIFT_RIGHT || keyCode == KeyEvent.KEYCODE_SHIFT_LEFT) {
+
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                //若为回车键，直接返回
+                mHandler.removeCallbacks(mScanningFishedRunnable);
+                mHandler.post(mScanningFishedRunnable);
+            } else {
+                //延迟post，若500ms内，有其他事件
+                mHandler.removeCallbacks(mScanningFishedRunnable);
+                mHandler.postDelayed(mScanningFishedRunnable, MESSAGE_DELAY);
+            }
+
+        }
+    }
+
+    //检查shift键
+    private void checkLetterStatus(KeyEvent event) {
+        int keyCode = event.getKeyCode();
+        if (keyCode == KeyEvent.KEYCODE_SHIFT_RIGHT || keyCode == KeyEvent.KEYCODE_SHIFT_LEFT) {
+            if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                //按着shift键，表示大写
+                mCaps = true;
+            } else {
+                //松开shift键，表示小写
                 mCaps = false;
             }
         }
     }
+
+
+    //获取扫描内容
+    private char getInputCode(KeyEvent event) {
+
+        int keyCode = event.getKeyCode();
+
+        char aChar;
+
+        if (keyCode >= KeyEvent.KEYCODE_A && keyCode <= KeyEvent.KEYCODE_Z) {
+            //字母
+            aChar = (char) ((mCaps ? 'A' : 'a') + keyCode - KeyEvent.KEYCODE_A);
+        } else if (keyCode >= KeyEvent.KEYCODE_0 && keyCode <= KeyEvent.KEYCODE_9) {
+            //数字
+            aChar = (char) ('0' + keyCode - KeyEvent.KEYCODE_0);
+        } else {
+            //其他符号
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_PERIOD:
+                    aChar = '.';
+                    break;
+                case KeyEvent.KEYCODE_MINUS:
+                    aChar = mCaps ? '_' : '-';
+                    break;
+                case KeyEvent.KEYCODE_SLASH:
+                    aChar = '/';
+                    break;
+                case KeyEvent.KEYCODE_BACKSLASH:
+                    aChar = mCaps ? '|' : '\\';
+                    break;
+                default:
+                    aChar = 0;
+                    break;
+            }
+        }
+
+        return aChar;
+
+    }
+
+
+
 
     public interface OnScanSuccessListener {
         public void onScanSuccess(String barcode);
@@ -98,7 +133,9 @@ public class ScanGunKeyEventHelper {
         mOnScanSuccessListener = null;
     }
 
-
 }
+
+
+
 
 
